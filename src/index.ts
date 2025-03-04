3f7e778b202793d8b4cdc9bb36866868f679b770
Sashikumar <yshashi30@gmail.com>
2025-03-05 02:18:59 +0800

    feat: added cors

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,23 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: env.CLIENT_URL,
+  origin: function(origin, callback) {
+    const allowedOrigins = [
+      'https://interviewhelper.in',
+      'http://localhost:4321',
+      env.CLIENT_URL
+    ].filter(Boolean);
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.warn(`CORS blocked request from origin: ${origin}`);
+      callback(null, false);
+    }
+  },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
